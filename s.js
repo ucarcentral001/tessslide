e9a45f0917b897e3bb85ba3f9d786fe1871319b6
ucarcentral001 <142104424+ucarcentral001@users.noreply.github.com>
2023-08-15 20:34:27 +0700

    Update s.js

--- s.js
+++ s.js
@@ -12,7 +12,6 @@ function currentSlide(n) {
 function showSlides(n) {
     let i;
     const slides = document.getElementsByClassName("separator");
-    const dots = document.getElementsByClassName("dot");
 
     if (n > slides.length) {
         slideIndex = 1;
@@ -23,12 +22,8 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+ }
 setInterval(function () {
     plusSlides(1);
 }, 5000);
